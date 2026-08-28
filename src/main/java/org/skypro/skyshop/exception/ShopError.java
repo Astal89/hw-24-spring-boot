42cf83f8ec4be42d19119003012c7c838722d956
astal <astalbox@gmail.com>
2026-08-28 13:22:11 +0300

    Spring exception: add solution

---
+++ src/main/java/org/skypro/skyshop/exception/ShopError.java
@@ -0,0 +1,19 @@
+package org.skypro.skyshop.exception;
+
+public class ShopError {
+    private final int code;
+    private final String message;
+
+    public ShopError(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
